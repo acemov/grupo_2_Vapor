32b58009836e5b3de82e8c8e1e13bbb1f6d753c5
noctis-fleuret <lucasdavlag@gmail.com>
2023-08-23 18:41:28 -0300

    actualizando entry point

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 // - - - CONFIGURACIONES - - - //
 
 const express = require('express')
-const path = require('path')
-
 const app = express()
-const publicPath = path.resolve(__dirname, '../public')
 
+const path = require('path')
+const publicPath = path.resolve(__dirname, '../public')
 app.use(express.static(publicPath))
+
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 // - - - IMPORTANDO RUTAS - - - //
 const rutaCarrito = require('./routes/carrito')
