d313f2500ef46b5e91d3da52b93cb74992c8bc03
FrancoEMTG <aesevv@gmail.com>
2023-08-23 10:30:27 -0300

    corrigiendo ruta de public

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const publicPath = path.resolve(__dirname, './public')
+const publicPath = path.resolve(__dirname, '../public')
 
 app.use(express.static(publicPath))
 app.set('view engine', 'ejs')
